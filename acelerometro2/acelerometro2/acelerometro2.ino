72a060b8bf5566934e5164001d03376c7d314c31
unknown <micro2@ele.puc-rio.br>
2025-06-18 18:52:34 -0300

    semana 2
    ME MATA 18:52

--- acelerometro2/acelerometro2/acelerometro2.ino
+++ acelerometro2/acelerometro2/acelerometro2.ino
@@ -1,26 +1,49 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
+#include <string.h>
 #include <Wire.h>
 #include <GFButton.h>
 
-#define ACEL_THRSHLD 5.0
-#define TAM 20
+#define GYRO_THRSHLD 0.5
+#define TAM_GRAVACAO 20
+#define TAM_GERAL (15 * TAM_GRAVACAO)
+#define PAUSA_MOV 300
+#define E 0.1 // = sqrt(0.1*0.1*3)
+
 
 Adafruit_MPU6050 mpu;
+const int MPU_INT_PIN = 4;
 
 GFButton botao1(4);
 GFButton botao2(15); 
 
-int i = 0;
-char gesto[TAM];
+int i = 0; // iterador gravação do gesto
+char gesto[TAM_GRAVACAO] = "yyyyyyy";
+
+int j = 0; // iterador gesto atual
+char gestoAtual[TAM_GERAL] = "xxxxxxxx";
 
 bool gravando = true;
+bool movimentando = true;
 
 const int interruptPin = 33;
+
 
 bool motionLast20seg = false;
 
 unsigned long instanteAnterior = 0;
+unsigned long instAntGesto = 0;
+
+bool movimentoAtivo = false;
+unsigned long tempoUltimoMovimento = 0;
+unsigned long tempoDeMovimento = 0;
+unsigned long inicioMovimento = 0;
+float x_media = 0, y_media = 0, z_media = 0;
+int soma = 0;
+
+float gx_calib;
+float gy_calib;
+float gz_calib;
 
 float x_offset = 0;
 float y_offset = 0;
@@ -28,37 +51,58 @@ float z_offset = 0;
 
 void btn1(GFButton &botao1){
   Serial.println(">> BOTAO 1 PRESSIONADO <<");
-  if(gravando){
-    gesto[i] = '\0';
+  if(gravando){ // para de gravar
     gravando = false;
-    i = 0;
-    for(int j = 0; gesto[j] != '\0'; j++){
-      Serial.print(gesto[j]); Serial.print(" ");
+    for(int K = 0; gesto[K] != '\0'; K++){
+      Serial.print(gesto[K]); Serial.print(" ");
     }
     Serial.println("");
   } 
-  else { gravando = true; }
+  else { // começa a gravar
+    gravando = true; 
+    i = 0;
+    strcpy(gesto, "yyyyy");
+
+  }
 }
 
 void btn2(GFButton &botao2){
   Serial.println(">> BOTAO 2 PRESSIONADO <<");
+  if(movimentando){
+    movimentando = false;
+    Serial.println(gesto);
+    Serial.println(gestoAtual);
+  } else{ 
+    movimentando = true; 
+    j = 0;
+    strcpy(gestoAtual, "xxxxx");
+  }
 }
 
-void gravacao(char mov){
+void movimento(char mov){
+  char movimento[2] = {mov, '\0'};
   if(gravando){
-    gesto[i] = mov;
-    i = (++i)%TAM;
+    // gesto[i] = mov;
+    strcpy(&gesto[i], movimento);
+    i = (++i)%(TAM_GRAVACAO - 1);
+    strcpy(gestoAtual, "xxxxx");
+  }
+  if(!gravando && movimentando){
+    strcpy(&gestoAtual[j], movimento);
+    j = (++j)%(TAM_GERAL - 1);
   }
 }
 
 void setup(void) {
   Serial.begin(115200);
+  Wire.begin(21, 22);
   while (!Serial)
     delay(10); // will pause Zero, Leonardo, etc until serial console opens
 
   Serial.println("Adafruit MPU6050 test!");
 
   // Try to initialize!
+  // if (!mpu.begin()) {
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
     while (1) {
@@ -69,128 +113,210 @@ void setup(void) {
 
   //setupt motion detection
   mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
-  mpu.setMotionDetectionThreshold(15);
-  mpu.setMotionDetectionDuration(50);
+  mpu.setMotionDetectionThreshold(3.5);
+  mpu.setMotionDetectionDuration(400);
   mpu.setInterruptPinLatch(true);	// Keep it latched.  Will turn off when reinitialized.
   mpu.setInterruptPinPolarity(true);
   mpu.setMotionInterrupt(true);
 
   botao1.setPressHandler(btn1);
+  // botao1.setReleaseHandler(btn1);
   botao2.setPressHandler(btn2);
+  // botao2.setReleaseHandler(btn2);
 
   Serial.println("");
-  delay(100);
+  const int CALIB_SAMPLES = 500;
+float gx_calib = 0, gy_calib = 0, gz_calib = 0;
+
+for(int i=0; i<CALIB_SAMPLES; i++) {
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+    gx_calib += g.gyro.x;
+    gy_calib += g.gyro.y;
+    gz_calib += g.gyro.z;
+    delay(5);
+}
+
+gx_calib /= CALIB_SAMPLES;
+gy_calib /= CALIB_SAMPLES;
+gz_calib /= CALIB_SAMPLES;
+
 }
 
 void loop() {
   botao1.process();
   botao2.process();
-  // sensors_event_t a, g, temp;
-  // mpu.getEvent(&a, &g, &temp);
+  sensors_event_t a, g, temp;
+  int x,y,z;
+  mpu.getEvent(&a, &g, &temp);
   // a.acceleration.x -= x_offset;
   // a.acceleration.y -= y_offset;
   // a.acceleration.z -= z_offset;
+  // x = a.acceleration.x;
+  // y = a.acceleration.y; 
+  // z = a.acceleration.z; 
+  x = g.gyro.x - gx_calib;
+  y = g.gyro.y - gy_calib;
+  z = g.gyro.z - gz_calib;
 
-  // Serial.print(a.acceleration.x);
-  // Serial.print(", Y: ");
-  // Serial.print(a.acceleration.y);
-  // Serial.print(", Z: ");
-  // Serial.print(a.acceleration.z);
-  // Serial.println(" m/s^2");
+  // Serial.println("--------------------");
+  // Serial.print(x);
+  // Serial.print(",\tY: ");
+  // Serial.print(y);
+  // Serial.print(",\tZ: ");
+  // Serial.print(z);
+  // Serial.println("\to/s");
+  // delay(30);
 
-  if(mpu.getMotionInterruptStatus()) {
-    motionLast20seg = true;
-    instanteAnterior = millis();
+// TODO : TESTAR COM ACELEROMETRO + GIROSCOPIO
 
-    /* Get new sensor events with the readings */
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
-    a.acceleration.x -= x_offset;
-    a.acceleration.y -= y_offset;
-    a.acceleration.z -= z_offset;
+  float magnitude = sqrt( x*x + y*y + z*z );
 
-    float x = a.acceleration.x;
-    float y = a.acceleration.y;
-    float z = a.acceleration.z;
-
-    char maior = 'w';
-
-    if(abs(x) >= abs(z) && abs(x) >= abs(y)){
-      maior = 'x';
-    } if(abs(z) >= abs(x) && abs(z) >= abs(y)){
-      maior = 'z';
-    } if(abs(y) >= abs(z) && abs(y) >= abs(x)){
-      maior = 'y';
-    }
-
-    if(x >= ACEL_THRSHLD && maior == 'x'){
-      Serial.println("----------------------------");
-      Serial.print(x); Serial.print("\t");
-      Serial.print(y); Serial.print("\t");
-      Serial.print(z); Serial.print("\t");
-      Serial.println("frente");
-      Serial.println("----------------------------");
-      gravacao('f');
-    } else if (x <= -ACEL_THRSHLD && maior == 'x'){
-      Serial.println("----------------------------");
-      Serial.print(x); Serial.print("\t");
-      Serial.print(y); Serial.print("\t");
-      Serial.print(z); Serial.print("\t");
-      Serial.println("tras");
-      gravacao('t');
-      Serial.println("----------------------------");
-    } else if (z <= -ACEL_THRSHLD && maior == 'z'){
-      Serial.println("----------------------------");
-      Serial.print(x); Serial.print("\t");
-      Serial.print(y); Serial.print("\t");
-      Serial.print(z); Serial.print("\t");
-      Serial.println("cima");
-      gravacao('c');
-      Serial.println("----------------------------");
-    } else if (z >= ACEL_THRSHLD && maior == 'z'){
-      Serial.println("----------------------------");
-      Serial.print(x); Serial.print("\t");
-      Serial.print(y); Serial.print("\t");
-      Serial.print(z); Serial.print("\t");
-      Serial.println("baixo");
-      gravacao('b');
-      Serial.println("----------------------------");
-    } else if (y <= -ACEL_THRSHLD && maior == 'y'){
-      Serial.println("----------------------------");
-      Serial.print(x); Serial.print("\t");
-      Serial.print(y); Serial.print("\t");
-      Serial.print(z); Serial.print("\t");
-      Serial.println("direita");
-      gravacao('d');
-      Serial.println("----------------------------");
-    } else if (y >= ACEL_THRSHLD && maior == 'y'){
-      Serial.println("----------------------------");
-      Serial.print(x); Serial.print("\t");
-      Serial.print(y); Serial.print("\t");
-      Serial.print(z); Serial.print("\t");
-      Serial.println("esquerda");
-      gravacao('e');
-      Serial.println("----------------------------");
-    } 
-  // reseta os valores do acelerometro
-  x_offset = a.acceleration.x;
-  y_offset = a.acceleration.y;
-  z_offset = a.acceleration.z;
+  if(!movimentoAtivo && magnitude > GYRO_THRSHLD){
+        movimentoAtivo = true;
+        inicioMovimento = millis();
+        x_media = 0;
+        y_media = 0;
+        z_media = 0;
+        soma = 0;
+        // Serial.println("Comecou o movimento");
+  }
+  if(movimentoAtivo && magnitude > E) {
+      // Serial.println("Esta rolando o movimento");
+    x_media += x;
+    y_media += y;
+    z_media += z;
+    soma++;
   }
 
-  else if (millis() - instanteAnterior > 500) {
+// TODO : RETESTAR COM ACELEROMETRO COM SOMA++ Q TAVA FALTANDO
+
+  // termino de movimento (acelerometro prox de zero)
+  if(magnitude < E && movimentoAtivo && (millis() - inicioMovimento > PAUSA_MOV)){
+    // Serial.println("TERMINOU O MOVIMENTO");
+    movimentoAtivo = false;
+
+    x_media = x_media / (float)soma;
+    y_media = y_media / (float)soma;
+    z_media = z_media / (float)soma;
+
+    // Determina direção predominante
+    char maior = 'w';
+    if (abs(x_media) >= abs(z_media) && abs(x_media) >= abs(y_media)) {
+        maior = 'x';
+    } else if (abs(z_media) >= abs(x_media) && abs(z_media) >= abs(y_media)) {
+        maior = 'z';
+    } else if (abs(y_media) >= abs(z_media) && abs(y_media) >= abs(x_media)) {
+        maior = 'y';
+    }
+
+    // Processa movimento baseado na média
+    if (x_media > 0 && maior == 'x') { // x_media >= GYRO_THRSHLD &&
+        Serial.println("frente");
+        movimento('f');
+    } else if (x_media < 0 && maior == 'x') { // x_media <= -GYRO_THRSHLD &&
+        Serial.println("tras");
+        movimento('t');
+    } else if (z_media < 0 && maior == 'z') { // z_media <= -GYRO_THRSHLD &&
+        Serial.println("cima");
+        movimento('c');
+    } else if (z_media > 0 && maior == 'z') { // z_media >= GYRO_THRSHLD &&
+        Serial.println("baixo");
+        movimento('b');
+    } else if (y_media < 0 && maior == 'y') { // y_media <= -GYRO_THRSHLD &&
+        Serial.println("direita");
+        movimento('d');
+    } else if (y_media > 0 && maior == 'y') { // y_media >= GYRO_THRSHLD &&
+        Serial.println("esquerda");
+        movimento('e');
+    }
+
+    // Atualiza offsets
+    // x_offset = a.acceleration.x;
+    // y_offset = a.acceleration.y;
+    // z_offset = a.acceleration.z;
+  }
+
+  // caso movimento muito curto
+  else if(magnitude < E && movimentoAtivo && (millis() - inicioMovimento <= PAUSA_MOV)){
+    // Serial.println("Terminou o movimento rapido demais");
+    movimentoAtivo = false;
+  }
+
+  // if(mpu.getMotionInterruptStatus() /*&& millis() - instAntGesto > PAUSA_MOV*/) {
+  //   motionLast20seg = true;
+  //   instanteAnterior = millis();
+  //   instAntGesto = millis();
+
+  //   /* Get new sensor events with the readings */
+  //   sensors_event_t a, g, temp;
+  //   mpu.getEvent(&a, &g, &temp);
+  //   a.acceleration.x -= x_offset;
+  //   a.acceleration.y -= y_offset;
+  //   a.acceleration.z -= z_offset;
+
+  //   float x = a.acceleration.x;
+  //   float y = a.acceleration.y;
+  //   float z = a.acceleration.z;
+
+  //   char maior = 'w';
+
+  //   // movimento em so uma direção
+  //   if(abs(x) >= abs(z) && abs(x) >= abs(y)){
+  //     maior = 'x';
+  //   } else if(abs(z) >= abs(x) && abs(z) >= abs(y)){
+  //     maior = 'z';
+  //   } else if(abs(y) >= abs(z) && abs(y) >= abs(x)){
+  //     maior = 'y';
+  //   }
+
+  //   if(x >= GYRO_THRSHLD && maior == 'x'){
+  //     Serial.println("frente");
+  //     movimento('f');
+  //   } else if (x <= -GYRO_THRSHLD && maior == 'x'){
+  //     Serial.println("tras");
+  //     movimento('t');
+  //   } else if (z <= -GYRO_THRSHLD && maior == 'z'){
+  //     Serial.println("cima");
+  //     movimento('c');
+  //   } else if (z >= GYRO_THRSHLD && maior == 'z'){
+  //     Serial.println("baixo");
+  //     movimento('b');
+  //   } else if (y <= -GYRO_THRSHLD && maior == 'y'){
+  //     Serial.println("direita");
+  //     movimento('d');
+  //   } else if (y >= GYRO_THRSHLD && maior == 'y'){
+  //     Serial.println("esquerda");
+  //     movimento('e');
+  //   } 
+  // // reseta os valores do acelerometro
+  // x_offset = a.acceleration.x;
+  // y_offset = a.acceleration.y;
+  // z_offset = a.acceleration.z;
+  // }
+
+  else if (millis() - instanteAnterior > 700) {
     motionLast20seg = false;
     instanteAnterior = millis();
  }
 
- if(!motionLast20seg){
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-  x_offset = a.acceleration.x;
-  y_offset = a.acceleration.y;
-  z_offset = a.acceleration.z;
+//  if(!motionLast20seg){
+//   sensors_event_t a, g, temp;
+//   mpu.getEvent(&a, &g, &temp);
+//   x_offset = a.acceleration.x;
+//   y_offset = a.acceleration.y;
+//   z_offset = a.acceleration.z;
+//  }
+
+ if(!gravando && movimentando){
+  if(strstr(gestoAtual, gesto) != NULL){
+    Serial.println("=================");
+    Serial.println("GESTO 1");
+    Serial.print(gesto);
+    Serial.println(gestoAtual);
+    Serial.println("=================");
+    strcpy(gestoAtual, "xxxxx");
+  }
  }
 
-
-  delay(10);
 }
